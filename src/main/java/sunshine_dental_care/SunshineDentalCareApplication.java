5d6fe043c63dea9d64bb96e8b34b03c937552872
TEMPP1901 <temppol1901@gmail.com>
2025-10-14 14:00:43 +0700

    Initial commit: Sunshine Dental Care Spring Boot project

---
+++ src/main/java/sunshine_dental_care/SunshineDentalCareApplication.java
@@ -0,0 +1,13 @@
+package sunshine_dental_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunshineDentalCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunshineDentalCareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sunshine_dental_care/SunshineDentalCareApplicationTests.java
@@ -0,0 +1,13 @@
+package sunshine_dental_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunshineDentalCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
