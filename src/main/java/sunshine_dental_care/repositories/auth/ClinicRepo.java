c8193e85173e41391b7efccee1488ab8ce469da6
Corn IT <ngominhtuan2109@gmail.com>
2025-12-06 23:07:46 +0700

    Phát triển tính năng Smart Chatbot tích hợp Generative AI (Google Gemini) sử dụng kỹ thuật RAG

--- src/main/java/sunshine_dental_care/repositories/auth/ClinicRepo.java
+++ src/main/java/sunshine_dental_care/repositories/auth/ClinicRepo.java
@@ -2,9 +2,8 @@ package sunshine_dental_care.repositories.auth;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import sunshine_dental_care.entities.Clinic;
-
-import java.util.Optional;
+import java.util.List;
 
 public interface ClinicRepo extends JpaRepository<Clinic, Integer> {
-
+    List<Clinic> findByIsActiveTrue();
 }
